a91c6ced23e6d304758550d68115c9748d1865b0
mquiroga15 <miquirog@ucm.es>
2025-09-25 17:47:45 +0200

    Update ISM paths

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = r"C:\EODP_all\eodp\auxiliary"
+indir = r"C:\EODP_all\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+outdir = r"C:\EODP_all\EODP-TS-ISM\myoutput"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
