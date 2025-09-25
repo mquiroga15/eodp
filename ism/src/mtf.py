19e4040a9a96fc422ea80d741198e96be45af678
mquiroga15 <miquirog@ucm.es>
2025-09-25 20:17:44 +0200

    MTF frequencies

--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -92,6 +92,19 @@ class mtf:
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
+        fstepAlt = 1 / nlines / w
+        fstepAct = 1 / ncolumns / w
+        eps = 1e-10
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt)
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct)
+
+        cutoff = D / lambd / focal
+        fnAlt = fAlt / cutoff
+        fnAct = fAct * w
+
+        [fnAltxx, fnActxx] = np.meshgrid(fnAlt, fnAct, indexing='ij')  # Please use ‘ij’ indexing or you will get the transpose
+        fn2D = np.sqrt(fnAltxx * fnAltxx + fnActxx * fnActxx)
+        fr2D = fn2D / cutoff / w
         return fn2D, fr2D, fnAct, fnAlt
 
     def mtfDiffract(self,fr2D):
