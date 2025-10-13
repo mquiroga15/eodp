42e1623395b2da2ec684e1817c9b91534cd63313
mquiroga15 <miquirog@ucm.es>
2025-10-13 19:51:13 +0200

    Find saturated pixels

--- ism/test/testISM0002.py
+++ ism/test/testISM0002.py
@@ -6,9 +6,11 @@ print('\nTEST ISM START\n')
 all_ok = True
 
 fail = []
+saturated = []
 ok = True
 print('Final TOA test')
 for band in range(4):
+    saturated_in_band = 0
     toa_ref = readToa(r"C:\EODP_all\EODP-TS-ISM\output", r"ism_toa_VNIR-"+str(band)+".nc")
     toa_own = readToa(r"C:\EODP_all\EODP-TS-ISM\myoutput", r"ism_toa_VNIR-"+str(band)+".nc")
     # 3-sigma limit
@@ -22,9 +24,18 @@ for band in range(4):
         fail.append(band)
     else:
         print(f'PASS: 3-sigma for channel {band} is {sig3:.5f}')
+
+    for pixel in toa_own.flatten():
+        if pixel >= 4095:
+            saturated_in_band += 1
+    saturated.append(saturated_in_band/(toa_own.shape[0]*toa_own.shape[1])*100)
+
 if ok:
     print(f'\nSUCCESS: all channels passed the final TOA test')
 else:
     print(f'ERROR: Some channels did not pass the final TOA test:')
     print(f'The following bands have failed: {fail}')
     all_ok = False
+
+print(f'\nSaturated pixels per band (%):')
+print(saturated)
